8b0d1cf8ea044fd542d9a3bce3d167c250fd6f21
esillen <esillen@kth.se>
2015-10-19 17:02:14 +0200

    cleaned up the particle filter code, fixed some issues. Still not in a working state

--- src/particle_filter_python.py
+++ src/particle_filter_python.py
@@ -1,7 +1,8 @@
-#If this works, we should rewrite it in c++!!!!!!!!!!!!!!!!!!!!!
+# TODO If this works, we should rewrite it in c++!!!!!!!!!!!!!!!!!!!!!
 
 import random
 import math
+import copy
 
 NUMBER_OF_PARTICLES = 1000
 VELOCITY_RANDOMNESS = 0
@@ -15,18 +16,17 @@ class Particle:
         self.y = 0
         self.w = 0
         self.probability = 0
+
     def update(self, vel, rotvel):
-        vel = vel + random.random()*VELOCITY_RANDOMNESS
-        self.w = self.w + rotvel + random.random()*ROTATION_RANDOMNESS
-        self.x = self.x + math.cos(self.w)
-        self.y = self.y + math.sin(self.w)
-
-
+        vel += random.random()*VELOCITY_RANDOMNESS
+        self.w += rotvel + random.random()*ROTATION_RANDOMNESS
+        self.x += vel*math.cos(self.w)
+        self.y += vel*math.sin(self.w)
 
 
 class Observation:
     def __init__(self):
-        #becomes world coordinates when combined with particle coordinates
+        # becomes world coordinates when combined with particle coordinates
         self.x = 0
         self.y = 0
         self.w = 0
@@ -34,77 +34,90 @@ class Observation:
         self.estimated_distance_left = 0
         self.estimated_distance_forward = 0
         self.estimated_distance_right = 0
+
     def get_line_from_observation(self):
-        farawayx = self.x + 100*math.cos(self.w)
-        farawayy = self.y + 100*math.sin(self.w)
-        return (self.x,self.y,farawayx,farawayy)
+        farawayx = self.x + 100.0*math.cos(self.w)
+        farawayy = self.y + 100.0*math.sin(self.w)
+        return self.x, self.y, farawayx, farawayy
 
 
-map = []
-particles = []
-for i in range(NUMBER_OF_PARTICLES):
-    particles.append(Particle())
-
-def particle_filter():
-    update()
-    get_probabilities()
-    resample()
+def particle_filter(observations, vvel, wvel):
+    update(vvel, wvel)
+    get_probabilities(observations)
+    resample(particles)
     return get_mean()
 
-def update(vel,wvel):
+
+def update(vel, wvel):
     for particle in particles:
-        particle.update(vel,wvel)
+        particle.update(vel, wvel)
 
 
 def get_probabilities(observations):
+    total_probability = 0.0
     for particle in particles:
-        #observation contains actual distance from sensor
+        # observation contains actual distance from sensor
         probability = 0.0
         for o in observations:
+
             o.x += particle.x
             o.y += particle.y
             wincrement = 0.05
             o.w += particle.w - wincrement
-            highestBeamProbability = 0.0
-            #Look at three "beams"
+            highest_beam_probability = 0.0
+            # Look at three "beams" TODO could be done with more or less beams
             for i in range(3):
 
                 closest = 100000.0
-                for wall in map:
-                    (ix,iy) = get_intersection(o.get_line_from_observation(),wall)
+                # TODO Should only look in a particular direction
+                for wall in wall_map:
+                    (ix, iy) = get_intersection(o.get_line_from_observation(), wall)
                     dist = math.sqrt((ix-o.x)*(ix-o.x) + (iy-o.y)*(iy-o.y))
                     if dist < closest:
                         closest = dist
-                #get probability that this is the correct distance
-                prob = asdfasdfasdf #TODO FIX ZIS, SOME KIND OF PROBABILITY BULLSHIT
-                if prob>highestBeamProbability:
-                    highestBeamProbability = prob
+                # get probability that this is the correct distance
+                # TODO really check if this is correct or not!!!!!!!!!!!!
+                prob = math.erf(closest/GAUSSIAN_STANDARD_DEVIATION) # TODO not sure if this is the correct use of the error function!!!!!
+                if prob > highest_beam_probability:
+                    highest_beam_probability = prob
                 o.w += wincrement
-            probability+=highestBeamProbability
-
-
-
-
-
-
-
-
-
-
-
-def resample():
+            probability += highest_beam_probability
+        particle.probability = probability
+        total_probability += probability
+    # Normalize
+    # TODO Could do resample() here as well for speed increase
     for particle in particles:
-        resample that shit
+        particle.probability = particle.probability/total_probability
 
-#takes two lines as input and returns the intersection of those lines in 2D
-def get_intersection((x11,y11,x12,y12),(x21,y21,x22,y22)):
+
+def resample(particles):
+    cumsum = 0.0
+    problist = []
+    for particle in particles:
+        cumsum += particle
+        problist.append(cumsum)
+
+    # This is N^2, is this really the best we can do???
+    for particle in particles:
+        random_number = random.random()
+        i = 0
+        for prob in problist:
+            if random_number > prob:
+                particle = copy.copy(particles[i])
+                break
+            i += 1
+
+
+# takes two lines as input and returns the intersection of those lines in 2D
+def get_intersection((x11, y11, x12, y12), (x21, y21, x22, y22)):
     k1 = (y12-y11)/(x12-x11)
     k2 = (y22-y21)/(x22-x21)
     m1 = y11-k1*x11
     m2 = y22-k2*x22
     x = (m2-m1)/(k1-k2)
     y = x*k1+m1
-    return (x,y)
+    return x, y
+
 
 def get_mean():
     x = 0
@@ -115,4 +128,10 @@ def get_mean():
         y += particle.y
         w += particle.w
 
-    return (x/NUMBER_OF_PARTICLES,y/NUMBER_OF_PARTICLES,w/NUMBER_OF_PARTICLES)
+    return x/NUMBER_OF_PARTICLES, y/NUMBER_OF_PARTICLES, w/NUMBER_OF_PARTICLES
+
+
+wall_map = []
+particles = []
+for counter in range(NUMBER_OF_PARTICLES):
+    particles.append(Particle())
