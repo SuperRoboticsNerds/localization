a08d53232692ceda0773694204d1e4a3438e0018
akash <akash.uee@gmail.com>
2015-11-07 01:30:38 +0100

    modifying distance converter

--- src/distance_converter.cpp
+++ src/distance_converter.cpp
@@ -4,22 +4,31 @@
 
 ros::Publisher distance_pub;
 
-double dist_conv(double x){
-    if(x<5.0) return 0.0;
-    return 0.000003188*x*x - 0.0034046669*x + 1.02568224;
+double long_dist_conv(double x){
+    if(x<15.0) return 0.0;
+    // return 0.000003188*x*x - 0.0034046669*x + 1.02568224;
+    return   0.000000109* pow(x, 3) -0.0000114150226132116*pow(x, 2) -0.0038 *x + 0.944;
 
 }
 
 
+
+double short_dist_conv(double x){
+    if(x<15.0) return 0.0;
+    return  0.0000558 * pow(x, 3) - 0.00704347775167042 * x*x  + 0.553495160550501;
+    // return 0.000003188*x*x - 0.0034046669*x + 1.02568224;
+
+}
+
 void adcallback(const ras_arduino_msgs::ADConverter::ConstPtr& msg){
 
     localization::Distance_message out;
-    out.d1 = dist_conv((double)msg->ch1);
-    out.d2 = dist_conv((double)msg->ch2);
-    out.d3 = dist_conv((double)msg->ch3);
-    out.d4 = dist_conv((double)msg->ch4);
-    out.d5 = dist_conv((double)msg->ch5);
-    out.d6 = dist_conv((double)msg->ch6);
+    out.d1 = short_dist_conv((double)msg->ch1);
+    out.d2 = short_dist_conv((double)msg->ch2);
+    out.d3 = short_dist_conv((double)msg->ch3);
+    out.d4 = short_dist_conv((double)msg->ch7);
+    out.d5 = short_dist_conv((double)msg->ch8);
+    out.d6 = long_dist_conv((double)msg->ch8);
     distance_pub.publish(out);
 }
 
@@ -28,7 +37,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "distance_converter");
     ros::NodeHandle n;
     distance_pub = n.advertise<localization::Distance_message>("/ir_measurements", 1000);
-    ros::Subscriber adc_sub = n.subscribe("/kobuki/adc", 1000, adcallback);
+    ros::Subscriber adc_sub = n.subscribe("/arduino/adc", 1000, adcallback);
     ros::spin();
     return 0;
 }
