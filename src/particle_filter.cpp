4ed15de3e280c118d75e0928fa2e4e0cbf795dbb
Erik Sillen <esillen@kth.se>
2015-11-29 22:35:35 +0100

    wassup

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -244,8 +244,8 @@ void get_predicted_observations(double px,double py, double th){
     double x,y,theta,x2,y2,distance,shortest;
     for (int i=0;i<NUM_OBSERVATIONS;i++){
         theta = th + sensor_positions[i][2];
-        x = px + sensor_positions[i][0]*cos(th) - sensor_positions[i][1]*sin(th);
-        y = py + sensor_positions[i][0]*sin(th) + sensor_positions[i][1]*cos(th);
+        x = px + sensor_positions[i][0]*cos(theta) - sensor_positions[i][1]*sin(theta);
+        y = py + sensor_positions[i][0]*sin(theta) + sensor_positions[i][1]*cos(theta);
 
         x2 = x + 100.0*cos(theta);
         y2 = y + 100.0*sin(theta);
@@ -414,6 +414,7 @@ void publish_mean(){
     position.theta = atan2(m_th_y,m_th_x);
 
     position_pub.publish(position);
+    /*
 
     visualization_msgs::Marker marker;
     marker.header.frame_id = "/map";
@@ -421,18 +422,17 @@ void publish_mean(){
 
     marker.ns = "basic_shapes";
     marker.id = 10;
-
-    marker.type = shape;
-
+    marker.type = visualization_msgs::Marker::ARROW;
     marker.action = visualization_msgs::Marker::ADD;
 
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
+    marker.pose.position.x = position.x;
+    marker.pose.position.y = position.y;
     marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
+
+    btQuaternion q;
+    btMatrix3x3(q).getRPY(0.0, 0.0, position.theta);
+
+    marker.pose.orientation = q;
 
     marker.scale.x = 1.0;
     marker.scale.y = 0.1;
@@ -442,6 +442,7 @@ void publish_mean(){
     marker.color.g = 1.0f;
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
+    */
 
     publishBeams(position.x, position.y, position.theta);
 }
